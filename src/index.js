e743e152174d05686dc58bb6ff42909cb2529f2f
carolina gomes de oliveira santos <carolinasamtos06@gmail.com>
2022-06-06 09:19:10 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import cors from 'cors'
     const server= express ();
          server.use (cors());
          server.listen (process.env.PORT [()=>])
-         console.log ('API online na porta $ {process.env PORT}'));
+         console.log("API online na porta ${process.env.PORT}");
 
